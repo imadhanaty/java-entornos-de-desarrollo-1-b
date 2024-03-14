25e18bdd14ef1ca6e229e722457985b5f331745f
imadhanaty <imad0006@alu.medac.es>
2024-03-14 14:13:10 +0100

    eclipse

---
+++ miprojecto/src/miprojecto/practica1/ejercicio1/Main.java
@@ -0,0 +1,10 @@
+package miprojecto.practica1.ejercicio1;
+
+class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hola zabi");
+	}
+
+}
